49a64e212594bd0bd423770c5dbb601fd1636b99
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2021-04-28 13:09:25 +1000

    Initial script for in-memory table profiling

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # hail-profile
+
+To create an SSH tunnel for YourKit, run:
+
+```bash
+gcloud compute ssh memtable -- -L 10001:localhost:10001
+```
+
---
+++ main.py
@@ -0,0 +1,22 @@
+import random
+import time
+import hail as hl
+
+PROF_ARGS = '-agentpath:/home/leo/YourKit-JavaProfiler-2021.3/bin/linux-x86-64/libyjpagent.so'
+
+SPARK_CONF = {
+    'spark.driver.extraJavaOptions': PROF_ARGS,
+    'spark.executor.extraJavaOptions': PROF_ARGS,
+}
+
+hl.init(default_reference='GRCh38', spark_conf=SPARK_CONF)
+
+ht = hl.read_table('/home/leo/gnomad_popmax_af.ht').cache()
+ht.count()  # Force cache to take effect.
+
+while True:
+    threshold = random.random() * 0.01
+    start_time = time.time()
+    print(threshold, ht.filter(ht.AF < threshold).count())
+    print(time.time() - start_time, 's')
+
