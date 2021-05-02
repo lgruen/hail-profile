4ae57e0db275a4453dd6ee6e727b06d9691612f1
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2021-05-02 23:11:41 +1000

    Fix handling of nulls

--- rdd.scala
+++ rdd.scala
@@ -1,21 +1,25 @@
-import is.hail.HailSuite
+import is.hail.HailContext
+import is.hail.backend.spark.SparkBackend
 import is.hail.expr.ir._
+import is.hail.variant.Locus
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.util.SizeEstimator
-import is.hail.variant.Locus
 
-val hs = new HailSuite
-val tir = TableRead.native(hs.fs, "/home/leo/gnomad_popmax_af.ht")
+val backend = SparkBackend(tmpdir = "/tmp", localTmpdir = "file:///tmp")
+val hc = HailContext(backend, logFile = "/tmp/hail.log")
+
+val tir = TableRead.native(backend.fs, "/home/leo/gnomad_popmax_af.ht")
 val tv = ExecuteContext.scoped() { ctx => Interpret(tir, ctx) }
 
-val floats = tv.rdd.map(row => {
+val doubles = tv.rdd.map(row => {
   if (row.isNullAt(2)) {
-    0.0f
+    Double.NaN
   } else {
-    row.getDouble(2).toFloat
+    row.getDouble(2)
   }
 })
-val cached = floats.persist(StorageLevel.MEMORY_ONLY)
+
+val cached = doubles.persist(StorageLevel.MEMORY_ONLY)
 println("Caching...")
 println("Count: " + cached.count())
 println("Size estimate: " + SizeEstimator.estimate(cached))
@@ -24,7 +28,7 @@ println("Filtering...")
 (1 to 9).foreach(t => {
   val threshold = t * 1e-4
   val t0 = System.nanoTime()
-  println(cached.filter(x => x < threshold).count)
+  println(threshold + " " + cached.filter(x => !x.isNaN && x < threshold).count)
   val t1 = System.nanoTime()
   println((t1 - t0) * 1e-9 + " s")
 })
