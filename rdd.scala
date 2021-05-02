1d80beac0eeaf5762826f87dee5f51a0d8fc4f56
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2021-05-02 19:32:50 +1000

    Only store index + float

--- rdd.scala
+++ rdd.scala
@@ -1,19 +1,32 @@
 import is.hail.HailSuite
 import is.hail.expr.ir._
 import org.apache.spark.storage.StorageLevel
+import org.apache.spark.util.SizeEstimator
+import is.hail.variant.Locus
 
 val hs = new HailSuite
 val tir = TableRead.native(hs.fs, "/home/leo/gnomad_popmax_af.ht")
 val tv = ExecuteContext.scoped() { ctx => Interpret(tir, ctx) }
-val cached = tv.rdd.persist(StorageLevel.MEMORY_ONLY)
+
+val floats = tv.rdd.map(row => {
+  var f = 0.0f
+  
+  if (!row.isNullAt(2))
+      f = row.getDouble(2).toFloat
+
+  val locus = row.get(0).asInstanceOf[Locus]
+  (locus.position, f)
+})
+val cached = floats.persist(StorageLevel.MEMORY_ONLY)
 println("Caching...")
-cached.count()
+println("Count: " + cached.count())
+println("Size estimate: " + SizeEstimator.estimate(cached))
 
 println("Filtering...")
 (1 to 9).foreach(t => {
   val threshold = t * 1e-4
   val t0 = System.nanoTime()
-  println(cached.filter(row => !row.isNullAt(2) && row.getDouble(2) < threshold).count)
+  println(cached.filter(x => x._2 < threshold).count)
   val t1 = System.nanoTime()
-  println((t1 - t0) * 1e9 + " s")
+  println((t1 - t0) * 1e-9 + " s")
 })
