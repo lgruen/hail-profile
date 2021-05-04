d06463cc9a29aea9c09cbfc9b1d4cfd60de89ac3
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2021-05-04 16:53:03 +1000

    Use Dask distributed scheduler

--- dask_filter.py
+++ dask_filter.py
@@ -1,9 +1,16 @@
 import time
 import dask.dataframe
+from dask.distributed import Client
 
-df = dask.dataframe.read_parquet('/home/leo/gnomad_popmax_af.parquet').persist()
-for t in range(1, 10):
-    threshold = t * 1e-4
-    start_time = time.time()
-    print(threshold, len(df[df.AF < threshold]))
-    print(time.time() - start_time, 's')
+def run():
+    client = Client(n_workers=32)
+    df = dask.dataframe.read_parquet('/home/leo/gnomad_popmax_af.parquet')
+    df = client.persist(df)
+    for t in range(1, 10):
+        threshold = t * 1e-4
+        start_time = time.time()
+        print(threshold, len(df[df.AF < threshold]))
+        print(time.time() - start_time, 's')
+
+if __name__ == '__main__':
+    run()
