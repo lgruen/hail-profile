f2f561a5de1240b3720a159caeeaaebdb7389912
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2021-06-29 22:52:35 +1000

    Simplify Parquet conversion

--- convert_to_parquet.py
+++ convert_to_parquet.py
@@ -1,16 +1,9 @@
 import hail as hl
-import pyarrow as pa
-import pyarrow.parquet as pq
-import pandas as pd
 
-SPARK_CONF = {
-    'spark.driver.memory': '100G',
-}
+hl.init(default_reference='GRCh38')
 
-hl.init(default_reference='GRCh38', spark_conf=SPARK_CONF)
-
-ht = hl.read_table('/home/leo/gnomad_popmax_af.ht')
-df = ht.to_pandas()
-table = pa.Table.from_pandas(df)
-pq.write_table(table, '/home/leo/gnomad_popmax_af.parquet')
+ht = hl.read_table('gs://leo-tmp-au/gnomad_popmax_af.ht')
+ht = ht.repartition(60)
+df = ht.to_spark()
+df.write.parquet('gs://leo-tmp-au/gnomad_popmax_af.parquet')
 
