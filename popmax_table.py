66617f3b96f637c5e500bda4e5fdd452ff3e1a51
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2021-06-29 22:20:28 +1000

    Don't rerun from all partitions

--- popmax_table.py
+++ popmax_table.py
@@ -5,4 +5,3 @@ ht = hl.read_table('gs://gcp-public-data--gnomad/release/3.1.1/ht/genomes/gnomad
 ht = ht.select(ht.popmax.AF)
 ht = ht.repartition(1000, shuffle=False)
 ht.write('gs://leo-tmp-au/gnomad_popmax_af.ht')
-print('number of partitions:', ht.n_partitions())
